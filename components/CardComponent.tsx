315d041802c32691e706cd2f3f81115645e78db1
Roman <71217095+sprigg4n-1@users.noreply.github.com>
2024-09-15 11:42:56 +0200

    feature: create card component, add font

---
+++ components/CardComponent.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+import React, { FC } from 'react';
+import Basket from '@/images/test-card-component/busket.svg';
+
+type CardProps = {
+  product_id: string;
+  model: string;
+  price: number;
+  photo_url: StaticImageData;
+  type: 'Watch' | 'Strap';
+  country: string;
+  case: string;
+  case_size: number;
+  case_color: string;
+  straps: string;
+  strap_color: string;
+  dial_color: string;
+  coating: string;
+  glass: string;
+  water_resistance: string;
+  movement: string;
+  instantaneus_rate: string;
+  standard_battery_life: string;
+};
+
+const handleAddToBasket = (
+  e: React.MouseEvent<HTMLButtonElement>,
+  id: string
+) => {
+  e.preventDefault();
+  console.log(`add to basket product with id: ${id}`);
+};
+
+const CardComponent: FC<CardProps> = ({
+  product_id,
+  model,
+  price,
+  photo_url,
+  type,
+}) => {
+  return (
+    <div className="flex flex-col items-center font-poppins">
+      <div className="relative group rounded-md overflow-hidden">
+        <Image
+          src={photo_url}
+          width={255}
+          height={300}
+          alt={`image of ${model}`}
+          className="object-fit w-[255px] h-[300px]"
+        />
+        <div className="absolute hidden top-0 left-0 w-full h-full group-hover:flex items-center justify-center flex-col gap-4 bg-amethyst bg-opacity-85">
+          <Image
+            src={Basket}
+            width={45}
+            height={45}
+            alt="basket image"
+            className="w-[45px] h-[45px]"
+          />
+          <button
+            onClick={(e) => handleAddToBasket(e, product_id)}
+            className="font-default text-white font-normal hover:underline">
+            Order {type}
+          </button>
+        </div>
+      </div>
+
+      <Link
+        href={`products/${product_id}`}
+        className="mt-5 mb-4 text-silver text-default hover:scale-110 hover:font-bold duration-300">
+        {model}
+      </Link>
+      <span className="font-normal text-xl text-onyx">${price}</span>
+    </div>
+  );
+};
+
+export default CardComponent;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        lato: ["Lato", "sans-serif"],
+        lato: ['Lato', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
       },
       fontSize: {
-        default: ["14px", { lineHeight: "18px" }],
-        xs: ["16px", { lineHeight: "22px" }],
-        xl: ["22px", { lineHeight: "32px" }],
+        default: ['14px', { lineHeight: '18px' }],
+        xs: ['16px', { lineHeight: '22px' }],
+        xl: ['22px', { lineHeight: '32px' }],
       },
       backgroundImage: {},
       boxShadow: {},
       dropShadow: {},
       colors: {
-        amethyst: "#7B51A6",
-        darkerAmethyst: "#6C399E",
-        silver: "#939393",
-        pearl: "#F9FAFE",
-        coral: "#FF8585",
-        onyx: "#000000",
-        snow: "#ffffff",
-        charcoal: "#202020",
+        amethyst: '#7B51A6',
+        darkerAmethyst: '#6C399E',
+        silver: '#939393',
+        pearl: '#F9FAFE',
+        coral: '#FF8585',
+        onyx: '#000000',
+        snow: '#ffffff',
+        charcoal: '#202020',
       },
       keyframes: {
         pulse: {},
@@ -35,21 +36,21 @@ const config: Config = {
       animation: {},
     },
     screens: {
-      mini: "520px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1440px",
-      "2xl": "1920px",
+      mini: '520px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+      '2xl': '1920px',
     },
     container: {
       center: true,
       padding: {
-        DEFAULT: "15px",
-        sm: "20px",
-        lg: "40px",
-        xl: "60px",
-        "2xl": "80px",
+        DEFAULT: '15px',
+        sm: '20px',
+        lg: '40px',
+        xl: '60px',
+        '2xl': '80px',
       },
     },
   },
