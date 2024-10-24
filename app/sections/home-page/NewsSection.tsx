d2b96b9d9d71ebd6de56351f1bd3389320dcf3f7
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2024-10-24 21:52:18 +0300

    Update NewsSection.tsx

--- app/sections/home-page/NewsSection.tsx
+++ app/sections/home-page/NewsSection.tsx
@@ -65,7 +65,6 @@ const NewsSection = () => {
             />
             <div className="relative">
               <Input
-                className="focus:outline-none focus:ring-1 focus:ring-onyx"
                 placeholder="Email"
                 type="email"
                 value={email}
