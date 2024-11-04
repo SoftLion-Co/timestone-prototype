fd355e27e5b754ef97a8d3d1a5dbb4420b0ae035
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-11-04 11:32:11 +0200

    feature: add new prop named scrollable

--- components/InputComponent.tsx
+++ components/InputComponent.tsx
@@ -1,15 +1,15 @@
-'use client';
-import React, { FC, useState, useRef, useEffect } from 'react';
-import { motion } from 'framer-motion';
+"use client";
+import React, { FC, useState, useRef, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import Image from 'next/image';
-import Arrow from '@/images/news-section/arrow.svg';
+import Image from "next/image";
+import Arrow from "@/images/news-section/arrow.svg";
 
 interface InputProps {
   placeholder?: string;
   className?: string;
-  type?: 'text' | 'password' | 'email' | 'search' | 'number';
-  background?: 'snow';
+  type?: "text" | "password" | "email" | "search" | "number";
+  background?: "snow";
   bordered?: boolean;
   fullWidth?: boolean;
   required?: boolean;
@@ -19,6 +19,7 @@ interface InputProps {
   showSelect?: boolean;
   options?: Option[];
   onSelect?: (value: string) => void;
+  scrollable?: boolean;
 }
 
 type Option = {
@@ -29,8 +30,8 @@ type Option = {
 const InputComponent: FC<InputProps> = ({
   placeholder,
   className,
-  type = 'text',
-  background = 'snow',
+  type = "text",
+  background = "snow",
   bordered = false,
   fullWidth = false,
   required = false,
@@ -40,11 +41,12 @@ const InputComponent: FC<InputProps> = ({
   onChange,
   options,
   onSelect,
+  scrollable = false,
 }) => {
-  const backgroundClass = bordered ? 'transparent' : background;
-  const textClass = background ? 'text-silver' : '';
-  const borderClass = bordered ? 'border border-whisper border-solid' : '';
-  const widthClass = fullWidth ? 'w-[100%]' : '';
+  const backgroundClass = bordered ? "transparent" : background;
+  const textClass = background ? "text-silver" : "";
+  const borderClass = bordered ? "border border-whisper border-solid" : "";
+  const widthClass = fullWidth ? "w-[100%]" : "";
 
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState<string | null>(null);
@@ -61,8 +63,8 @@ const InputComponent: FC<InputProps> = ({
         setIsOpen(false);
       }
     };
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
   const handleSelect = (value: string) => {
@@ -77,7 +79,8 @@ const InputComponent: FC<InputProps> = ({
         <div className="relative w-[272px] mini:w-[320px]" ref={selectRef}>
           <div
             className="border border-gray-300 rounded-lg py-[15px] px-[30px] cursor-pointer bg-snow text-silver"
-            onClick={toggleDropdown}>
+            onClick={toggleDropdown}
+          >
             {selected
               ? options?.find((option) => option.value === selected)?.label
               : placeholder}
@@ -87,23 +90,27 @@ const InputComponent: FC<InputProps> = ({
               alt="Arrow"
               width={14}
               className={`absolute right-[25px] top-[25px] transition-transform ${
-                isOpen ? 'rotate-180' : 'rotate-0'
+                isOpen ? "rotate-180" : "rotate-0"
               }`}
             />
           </div>
 
           {isOpen && (
             <motion.ul
-              className="absolute mt-2 w-full border border-gray-300 rounded-lg bg-snow z-10"
+              className={`absolute mt-2 w-full border border-gray-300 rounded-lg bg-snow z-10 ${
+                scrollable ? "max-h-[160px] overflow-y-auto" : ""
+              }`}
               initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -10 }}
-              transition={{ duration: 0.3 }}>
+              transition={{ duration: 0.3 }}
+            >
               {options?.map((option) => (
                 <li
                   key={option.value}
                   className="p-2 hover:bg-gray-200 cursor-pointer text-silver rounded-lg"
-                  onClick={() => handleSelect(option.value)}>
+                  onClick={() => handleSelect(option.value)}
+                >
                   {option.label}
                 </li>
               ))}
