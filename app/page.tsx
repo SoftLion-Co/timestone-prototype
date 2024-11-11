22ba7b67aa4828b6ba434da2dd93f59ee4932859
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2024-11-11 20:26:29 +0100

    fix: change some styles in hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import SliderSection from "@/app/sections/home-page/WatchesSliderSection";
 import OurMissionSection from "@/app/sections/home-page/OurMissionSection";
 import NewsSection from "./sections/home-page/NewsSection";
+import HeroSection from "./sections/home-page/HeroSection";
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <SliderSection />
       <OurMissionSection />
-      <NewsSection/>
+      <NewsSection />
     </>
   );
 }
