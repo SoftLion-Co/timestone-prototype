1c101b3661a94c9e1c166bc36be84dad2bf17f97
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-11-11 23:13:26 +0200

    refactor: move test objects to test folder

--- app/sections/home-page/WatchesSliderSection.tsx
+++ app/sections/home-page/WatchesSliderSection.tsx
@@ -6,11 +6,9 @@ import { Carousel } from "@mantine/carousel";
 import "@mantine/carousel/styles.css";
 
 import Button from "@/components/ButtonComponent";
-
 import ImgArrow from "@/images/vectors/arrow.svg";
-import TestImgWatch1 from "@/test/images/watches/watch-1.png";
-import TestImgWatch2 from "@/test/images/watches/watch-2.png";
-import TestImgWatch3 from "@/test/images/watches/watch-3.png";
+
+import { TestSliderProducts } from "@/test/sliderProducts"
 
 const textSliderObject = {
   title: "Watches",
@@ -20,47 +18,8 @@ const textSliderObject = {
     "multiple stylish strap combinations, and customized engraving options.",
 };
 
-const TestWatchesImages = [
-  {
-    src: TestImgWatch1,
-    name: "Volumenzeit S 2",
-    price: "$15.558",
-    createdAt: "2024-04-24T13:37:15Z",
-  },
-  {
-    src: TestImgWatch2,
-    name: "Volumenzeit S 6",
-    price: "$20.558",
-    createdAt: "2024-04-23T13:37:15Z",
-  },
-  {
-    src: TestImgWatch3,
-    name: "Volumenzeit S 7",
-    price: "$17.558",
-    createdAt: "2024-04-22T13:37:15Z",
-  },
-  {
-    src: TestImgWatch1,
-    name: "Volumenzeit S 2",
-    price: "$11.558",
-    createdAt: "2024-04-21T13:37:15Z",
-  },
-  {
-    src: TestImgWatch2,
-    name: "Volumenzeit S 6",
-    price: "$22.558",
-    createdAt: "2024-04-20T13:37:15Z",
-  },
-  {
-    src: TestImgWatch3,
-    name: "Volumenzeit S 7",
-    price: "$13.558",
-    createdAt: "2024-04-19T13:37:15Z",
-  },
-];
 
-//обмеження на 12 штук нових товарів
-const SortAndLimitProducts = (items: typeof TestWatchesImages) => {
+const SortAndLimitProducts = (items: typeof TestSliderProducts) => {
   return items
     .sort(
       (a, b) =>
@@ -114,7 +73,7 @@ const SliderSection = () => {
               </div>
             }
           >
-            {SortAndLimitProducts(TestWatchesImages).map((item, index) => (
+            {SortAndLimitProducts(TestSliderProducts).map((item, index) => (
               <Carousel.Slide
                 key={index}
                 className="flex flex-col items-center text-center"
---
+++ test/sliderProducts.ts
@@ -0,0 +1,43 @@
+import TestImgWatch1 from "@/test/images/watches/watch-1.png";
+import TestImgWatch2 from "@/test/images/watches/watch-2.png";
+import TestImgWatch3 from "@/test/images/watches/watch-3.png";
+
+
+export const TestSliderProducts = [
+    {
+      src: TestImgWatch1,
+      name: "Volumenzeit S 2",
+      price: "$15.558",
+      createdAt: "2024-04-24T13:37:15Z",
+    },
+    {
+      src: TestImgWatch2,
+      name: "Volumenzeit S 6",
+      price: "$20.558",
+      createdAt: "2024-04-23T13:37:15Z",
+    },
+    {
+      src: TestImgWatch3,
+      name: "Volumenzeit S 7",
+      price: "$17.558",
+      createdAt: "2024-04-22T13:37:15Z",
+    },
+    {
+      src: TestImgWatch1,
+      name: "Volumenzeit S 2",
+      price: "$11.558",
+      createdAt: "2024-04-21T13:37:15Z",
+    },
+    {
+      src: TestImgWatch2,
+      name: "Volumenzeit S 6",
+      price: "$22.558",
+      createdAt: "2024-04-20T13:37:15Z",
+    },
+    {
+      src: TestImgWatch3,
+      name: "Volumenzeit S 7",
+      price: "$13.558",
+      createdAt: "2024-04-19T13:37:15Z",
+    },
+  ];
