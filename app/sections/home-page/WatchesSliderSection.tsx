d4233c7135f627d9880273b6ccfd4ddcbedf961b
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-11-25 19:28:41 +0200

    fix(slider): connect button to catalog

--- app/sections/home-page/WatchesSliderSection.tsx
+++ app/sections/home-page/WatchesSliderSection.tsx
@@ -141,8 +141,10 @@ const SliderSection = () => {
             </Carousel>
           </div>
           <Button
-            text="Design Your Watch"
-            className="mt-[80px] mx-auto sm:mr-[20px] sm:ml-auto sm:mt-[90px] lg:mr-[60px]"
+            text="Your wathes"
+            tag="a"
+            href="/catalog"
+            className="mt-[80px] max-w-[200px] mx-auto sm:mr-[20px] sm:ml-auto sm:mt-[90px] lg:mr-[60px]"
           />
         </div>
       </section>
