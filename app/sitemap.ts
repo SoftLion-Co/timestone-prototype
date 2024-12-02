7c9a80ebed6cfda42a31289c64982680a8d4152a
sasha55sh <150395476+sasha55sh@users.noreply.github.com>
2024-12-02 13:03:58 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,13 @@
 import { MetadataRoute } from "next";
+import { CardProps } from "@/config/types";
+import { getProducts } from "@/services/ProductService";
 
-export default function sitemap() {
+export default async function sitemap() {
+  const products = await getProducts();
+  const getTitle = products.map((product: CardProps) => ({
+    url: `https://timestone.com/catalog/${product.handle}`,
+    lastModified: new Date(),
+  }));
   return [
     { url: "https://timestone.com", lastModified: new Date() },
     { url: "https://timestone.com/catalog", lastModified: new Date() },
@@ -8,6 +15,8 @@ export default function sitemap() {
     { url: "https://timestone.com/contact-us", lastModified: new Date() },
     { url: "https://timestone.com/auth", lastModified: new Date() },
     { url: "https://timestone.com/", lastModified: new Date() },
+    ...getTitle,
+
     //зробити функція, яка витягагає хендл назви продукту
   ];
 }
