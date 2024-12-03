49f58dd8ccf75b0e0f15a86ed0305322cdce3f69
Lyzen Vira <lyzen20vm@gmail.com>
2024-12-03 16:58:57 +0200

    fix sitemap again

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,12 +2,15 @@ import { Product } from "@/config/types";
 import { getProducts } from "@/services/ProductService";
 
 export default async function sitemap() {
-  const products: Product[] = await getProducts();
-  
-  const getTitle = products.map((product: Product) => ({
+  const data = await getProducts();
+  let getTitle =[{}];
+
+  if(data){
+  getTitle = data.products.map((product: Product) => ({
     url: `https://timestone.com/catalog/${product.handle}`,
     lastModified: new Date(),
   }));
+}
   return [
     { url: "https://timestone.com", lastModified: new Date() },
     { url: "https://timestone.com/catalog", lastModified: new Date() },
