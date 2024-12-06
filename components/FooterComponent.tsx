3e7de81aa55ac74be73179f44946b6316a611f6e
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2024-12-06 15:16:42 +0100

    fix: some style on account, footer, title

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
   return (
     <footer className="flex flex-col items-center gap-[50px] pt-[70px]">
       <Link href="/">
-        <Image src={Logo} alt="Logo" width={380} className="w-[380px]" />
+        <Image src={Logo} alt="Logo"  width={380} height={80} className="w-[380px] h-[80px]"/>
       </Link>
 
-      <p className="bg-pearl w-[100%] py-[20px] text-center text-silver">
+      <p className="bg-pearl w-[100%] py-[20px] text-center text-silver min-h-[60px] leading-[25px]">
         © Copyright {currentYear} TIMESTONE - All rights reserved.
       </p>
     </footer>
