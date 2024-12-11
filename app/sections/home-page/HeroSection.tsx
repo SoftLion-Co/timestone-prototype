6a3a8cf778cc4386f864e2bf9843cfd00ae6b2a4
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2024-12-11 13:07:50 +0100

    refactor: gap in  hero section

--- app/sections/home-page/HeroSection.tsx
+++ app/sections/home-page/HeroSection.tsx
@@ -58,7 +58,7 @@ const Links: FC<HeroProps> = ({ className }) => {
 const HeroSection: FC = () => {
   return (
     <section className="flex flex-col relative py-[40px] lg:py-[60px]">
-      <div className="flex flex-col items-center mx-[20px] lg:mr-[60px] gap-[40px] lg:flex-row lg:gap-[90px] lg:items-start xl:items-center">
+      <div className="flex flex-col items-center mx-[20px] lg:mr-[60px] gap-[40px] lg:flex-row lg:gap-[60px] lg:items-start xl:items-center">
         <Links className="hidden lg:grid" />
 
         <div className="text-center flex flex-col items-center lg:flex-col lg:text-left lg:items-start mini:w-[400px] lg:w-[570px]">
