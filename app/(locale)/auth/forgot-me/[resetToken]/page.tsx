5ceae3c7dc3fd749e53934cfca743fe89027db92
Yelyzaaveta <121977737+Yelyzaaveta@users.noreply.github.com>
2024-12-13 17:47:43 +0200

    chore: small update

---
+++ app/(locale)/auth/forgot-me/[resetToken]/page.tsx
@@ -0,0 +1,9 @@
+const Page = ({ params }: { params: any }) => {
+	const activatedToken = params.resetToken;
+  return (
+    <>
+    </>
+  );
+};
+
+export default Page;
---
+++ app/(locale)/auth/forgot-me/page.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TIMESTONE - Відновлення пароля",
+  description: "Сторінка відновлення пароля",
+  icons: { icon: "@/app/favicon.ico" },
+  viewport: { initialScale: 1.0, width: "device-width" },
+};
+
+const Page = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Page;
