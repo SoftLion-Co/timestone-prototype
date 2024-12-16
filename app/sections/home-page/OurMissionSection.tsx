f3fb6809ee3bd0a90510ba4f122326bef58ed160
AnastasiiaMoskaliuk <126007244+AnastasiiaMoskaliuk@users.noreply.github.com>
2024-12-16 16:14:13 +0100

    refactor: our mission

--- app/sections/home-page/OurMissionSection.tsx
+++ app/sections/home-page/OurMissionSection.tsx
@@ -17,7 +17,7 @@ const OurMissionSection = () => {
             loading="lazy"
           />
 
-          <div className="absolute top-0 -right-[4px] bg-gradient-to-l from-onyx to-transparent h-full w-[110px] hidden md:block" />
+          <div className="absolute block w-full rotate-180 -bottom-1 bg-gradient-to-b from-onyx to-transparent h-[60px] block right-0 md:rotate-0 md:top-0 md:-right-[4px] md:bg-gradient-to-l md:h-full md:w-[110px] " />
 
           <div
             className="absolute -bottom-[4px] w-[600px] h-[76px] bg-snow hidden md:block lg:w-[800px]"
@@ -26,10 +26,10 @@ const OurMissionSection = () => {
         </div>
 
         <div className="gap-[40px] py-[50px] mx-[18px] text-snow flex flex-col text-center items-center md:text-start md:items-start md:w-[328px] lg:w-[400px] md:mx-0 md:py-0 md:text-right">
-          <h1 className="font-frontrunner  leading-[50px] text-[28px] md:text-[32px] lg:text-[48px]">
+          <h1 className="font-frontrunner  leading-[50px] text-[28px] md:text-[35px] lg:text-[45px]">
           Наше призначення
           </h1>
-          <div className="flex flex-col font-poppins text-default gap-[10px]">
+          <div className="flex w-[80%] md:w-full flex-col font-poppins text-default gap-[10px]">
             <p> Наш магазин спеціалізується на продажі стильних та надійних годинників, які підходять для будь-якої ситуації — від повсякденних до урочистих моментів.</p>
             <p> Ми прагнемо дарувати нашим клієнтам не тільки точність, але й емоційний зв’язок із часом, роблячи кожен момент особливим.</p>
           </div>
